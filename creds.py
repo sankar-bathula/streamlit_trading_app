9fc6f1a58bf4ef84010101b2d394d6cae677c623
Shankar Bathula <41106397+sankar-bathula@users.noreply.github.com>
2026-04-21 21:37:32 +0530

    Clear sensitive data from creds.py
    Removed sensitive credentials from creds.py.

--- creds.py
+++ creds.py
@@ -1,8 +1,8 @@
 # Angel One SmartAPI Credentials
-api_key = "6ybPZHNp"         # API Key from Angel One developer portal
-client_code = "B352053" # Your Angel One client ID (e.g., A123456)
-client_pin = "9046"         # Your 4-digit MPIN
-totp_code = "H6D2W7QO7EEG3JOKCVVZVZIQYQ"     # The 32-character TOTP seed (not the 6-digit code)
+api_key = ""         # API Key from Angel One developer portal
+client_code = "" # Your Angel One client ID (e.g., A123456)
+client_pin = ""         # Your 4-digit MPIN
+totp_code = ""     # The 32-character TOTP seed (not the 6-digit code)
 
 # Telegram Bot (Optional for notifications)
 telegram_bot_token = ""
